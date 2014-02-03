ecae1b4c41560f9e188370e73968262e42e40053
jpkole <jp@kole.com>
2014-02-02 21:32:31 -0700

    step 2 working.

--- session_3_basic_grid/BasicRallyGrid/App.js
+++ session_3_basic_grid/BasicRallyGrid/App.js
@@ -9,16 +9,45 @@ Ext.define('CustomApp', {
     // Entry Point to App
     launch: function() {
 
-      console.log('our first app');     // see console api: https://developers.google.com/chrome-developer-tools/docs/console-api
-      this._loadData();                 // we need to prefix with 'this.' so we call a method found at the app level.
+      console.log('our second app');      // see console api: https://developers.google.com/chrome-developer-tools/docs/console-api
+      this._loadIterations();             // we need to prefix with 'this.' so we call a method found at the app level.
     },
 
-    // Get data from Rally
+    // Get iteration data from Rally
+    _loadIterations: function() {
+
+      this.iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+        listeners: {
+            ready: function(combobox) {
+                this._loadData();
+            },
+            select: function(combobox, records) {
+                this._loadData();
+            },
+            scope: this
+        }
+      });
+
+      this.add(this.iterComboBox);
+
+    },
+
+    // Get defect data from Rally
     _loadData: function() {
 
+      var selectedIterRef = this.iterComboBox.getRecord().get('_ref');
+      console.log('selected iter: ', selectedIterRef);
+
       var myStore = Ext.create('Rally.data.wsapi.Store', {
-          model: 'User Story',
+          model: 'Defect',
           autoLoad: true,                         // <----- Don't forget to set this to true! heh
+          filters: [
+            {
+              property: 'Iteration',
+              operation: '=',
+              value: selectedIterRef
+            }
+          ],
           listeners: {
               load: function(myStore, myData, success) {
                   console.log('got data!', myStore, myData);
@@ -26,24 +55,22 @@ Ext.define('CustomApp', {
               },
               scope: this                         // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
           },
-          fetch: ['FormattedID', 'Name', 'ScheduleState']   // Look in the WSAPI docs online to see all fields available!
+          fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']   // Look in the WSAPI docs online to see all fields available!
         });
 
     },
 
-    // Create and Show a Grid of given stories
-    _loadGrid: function(myStoryStore) {
+    // Create and Show a Grid of defects
+    _loadGrid: function(myDefectStore) {
 
       var myGrid = Ext.create('Rally.ui.grid.Grid', {
-        store: myStoryStore,
+        store: myDefectStore,
         columnCfgs: [         // Columns to display; must be the same names specified in the fetch: above in the wsapi data store
-          'FormattedID', 'Name', 'ScheduleState'
+          'FormattedID', 'Name', 'Severity', 'Iteration'
         ]
       });
 
       this.add(myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
-
-      console.log('what is this?', this);
 
     }
 
